a007631d5c6141caf377868bdad015665db913f7
justin <justinledford@gmail.com>
2018-03-02 11:16:39 -0600

    fixes

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ def l2_loss(x):
     return K.sum(K.square(x)) / 2
 
 class EncoderDecoder:
-    def __init__(self, input_shape, target_layer):
+    def __init__(self, input_shape=(256, 256, 3), target_layer=5):
         self.input_shape = input_shape
         self.target_layer = target_layer
         self.encoder = VGG19(input_shape=input_shape, target_layer=target_layer)
@@ -27,14 +27,18 @@ class EncoderDecoder:
         def loss(img_in, img_out):
             encoding_in = get_encodings(img_in)
             encoding_out = get_encodings(img_out)
-            l2_loss(img_out - img_in) + LAMBDA*l2_loss(encoding_out - encoding_in)
+            return l2_loss(img_out - img_in) + \
+                   LAMBDA*l2_loss(encoding_out - encoding_in)
         return loss
 
     def create_decoder(self, target_layer):
         layers = decoder_layers(self.encoder.output, target_layer)
-        return Conv2D(3, (3, 3), activation='relu', padding='same', name='decoder_out')(layers)
+        return Conv2D(3, (3, 3), activation='relu', padding='same',
+                name='decoder_out')(layers)
 
     def export_decoder(self):
         pass
 
 
+#ed = EncoderDecoder()
+#ed.model.summary()
