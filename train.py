f5f51ed061d4ece7e96062cee8446cbc79b84646
Justin Ledford <justinledford@gmail.com>
2018-03-04 15:19:29 -0600

    add create_gen

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from model import EncoderDecoder
 from util import count_num_samples
 
 TRAIN_PATH = 'data'
-TARGET_SIZE = (256, 256, 3)
+TARGET_SIZE = (256, 256)
 BATCH_SIZE = 4
 epochs = 1
 
@@ -12,8 +12,27 @@ datagen = ImageDataGenerator()
 gen = datagen.flow_from_directory(TRAIN_PATH, target_size=TARGET_SIZE,
                                   batch_size=BATCH_SIZE, class_mode=None)
 
+
+def create_gen(img_dir, target_size, batch_size):
+    datagen = ImageDataGenerator()
+    gen = datagen.flow_from_directory(img_dir, target_size=target_size,
+                                      batch_size=batch_size, class_mode=None)
+
+    def tuple_gen():
+        for img in gen:
+            if img.shape[0] != batch_size:
+                continue
+
+            # (X, y)
+            yield (img, img)
+
+    return tuple_gen()
+
+
+gen = create_gen(TRAIN_PATH, TARGET_SIZE, BATCH_SIZE)
+
 num_samples = count_num_samples(TRAIN_PATH)
-steps_per_epoch = num_samples // batch_size
+steps_per_epoch = num_samples // BATCH_SIZE
 
 encoder_decoder = EncoderDecoder()
 
